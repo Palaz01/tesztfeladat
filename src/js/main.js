d673378f57c0785ebf08ed97c0012cec2d8fb514
palaz <barnabas.palatinus@gmail.com>
2019-05-23 13:52:48 +0200

    basic form validation

--- src/js/main.js
+++ src/js/main.js
@@ -2,17 +2,50 @@ $(document).ready(function () {
    /******************************
        BOTTOM SCROLL TOP BUTTON
     ******************************/
-
-   // declare variable
    var scrollTop = $(".scrollTop");
 
-   //Click event to scroll to top
-   $(scrollTop).click(function(event) {
+   $(scrollTop).click(function (event) {
       event.preventDefault();
       $('html, body').animate({
          scrollTop: 0
       }, 800);
       return false;
-   }); // click() scroll top EMD
+   });
+});
 
-}); // ready() END
+/******************************
+ BASIC FORM VALIDATION
+******************************/
+
+var main = function () {
+   $('form').submit(function () {
+      var firstName = $('#firstname').val();
+
+      if (firstName === "") {
+         $('.firstname-error').text('Please enter your first name.')
+      }
+
+      return false;
+   });
+
+   $('form').submit(function () {
+      var lastName = $('#lastname').val();
+
+      if (lastName === "") {
+         $('.lastname-error').text('Please enter your last name.')
+      }
+
+      return false;
+   });
+
+   $('form').submit(function () {
+      var email = $('#email').val();
+
+      if (email === "") {
+         $('.email-error').text('Please enter your email address.')
+      };
+      return false;
+   })
+}
+
+$(document).ready(main);
