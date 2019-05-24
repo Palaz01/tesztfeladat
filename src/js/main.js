6ed34bb11fd496d15c7492eef0b4a47b409f00af
palaz <barnabas.palatinus@gmail.com>
2019-05-24 14:09:47 +0200

    navigation dropdown menu, nav-link hover fix

--- src/js/main.js
+++ src/js/main.js
@@ -49,3 +49,16 @@ var main = function () {
 }
 
 $(document).ready(main);
+
+/******************************
+ FOR DROPDOWN ON HOVER
+******************************/
+
+$(document).ready(function () {
+   $(".dropdown, .btn-group").hover(function () {
+      var dropdownMenu = $(this).children(".dropdown-menu");
+      if (dropdownMenu.is(":visible")) {
+         dropdownMenu.parent().toggleClass("open");
+      }
+   });
+});
